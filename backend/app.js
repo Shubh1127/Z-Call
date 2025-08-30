e109979dc18ea34ae25adc49627ffe8d6f89600b
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-08-30 23:16:35 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,31 @@
 import mongoose from "mongoose"
 import express from "express"
-import {clientServer} from "node:http"
-import {server} from "socket.io
+import dotenv from "dotenv"
+import { createServer } from "node:http"
+import {Server} from "socket.io"
 import cors from "cors"
+
+dotenv.config();
+
+const app = express();
+const server=createServer(app);
+const io = new Server(server);
+
+app.get('/',(req,res)=>{
+    return res.json("hello World")
+})
+
+const start=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to Database");
+        server.listen(8000,()=>{
+            console.log("Server started on port 8000");
+        })
+    }catch(err){
+        console.log(err.message)
+        // return res.status(500).json({error: err.message});
+    }
+}
+
+start();
