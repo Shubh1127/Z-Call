fefbed23ec0b7cdd7424514bcba559a27515e0a6
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-09-05 19:11:45 +0530

    working on models and socketmanager

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -1,6 +1,24 @@
 import {Server} from "socket.io"
 
+let connections={};
+let messages={};
+let timeOnline={};
+
 const connectToSocket=(server)=>{
-   return new Server(server);
+
+   const io=new Server(server);
+   io.on("connection",(socket)=>{
+         console.log("New client connected",socket.id);
+
+         socket.on("join-room",(path)=>{
+
+         })
+
+         socket.on("signal",(toId,message)=>{
+            io.to(told).emit("signal",socket.id,message);
+         })
+         
+   })
+   return io;
 }
 export default connectToSocket;
